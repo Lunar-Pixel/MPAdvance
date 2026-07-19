0d51be860d4edc11ea9f9e6c3d2c08a835b963d7
testyourmine <25854988+testyourmine@users.noreply.github.com>
2026-07-19 09:59:34 -0500

    Import m4a_tables.c

--- data/data.s
+++ data/data.s
@@ -1,7 +1,7 @@
 	.INCLUDE "gba.inc"
 	.INCLUDE "macro.inc"
 
-	.SECTION .data
+	.SECTION .rodata
 	@.SECTION .text
 
 	.GLOBAL game_data_08076ae8
@@ -322,107 +322,3 @@ gUnknown_0808E9AC:
 	.GLOBAL gUnknown_0808E9BC @ Town Game Room B amp sprite pointers
 gUnknown_0808E9BC:
 	.INCBIN "baserom.gba", 0x8E9BC, 0x1AEE9C-0x8E9BC
-
-	.GLOBAL gMPlayJumpTableTemplate
-gMPlayJumpTableTemplate:
-	.INCBIN "baserom.gba", 0x1AEE9C, 1
-
-	.INCBIN "baserom.gba", 0x1AEE9D, 0x1AEF2C-0x1AEE9D
-
-	.GLOBAL gScaleTable
-gScaleTable:
-	.INCBIN "baserom.gba", 0x1AEF2C, 1
-
-	.INCBIN "baserom.gba", 0x1AEF2D, 0x1AEFE0-0x1AEF2D
-
-	.GLOBAL gFreqTable
-gFreqTable:
-	.INCBIN "baserom.gba", 0x1AEFE0, 1
-
-	.INCBIN "baserom.gba", 0x1AEFE1, 0x1AF010-0x1AEFE1
-
-	.GLOBAL gPcmSamplesPerVBlankTable
-gPcmSamplesPerVBlankTable:
-	.INCBIN "baserom.gba", 0x1AF010, 1
-
-	.INCBIN "baserom.gba", 0x1AF011, 0x1AF028-0x1AF011
-
-	.GLOBAL gCgbScaleTable
-gCgbScaleTable:
-	.INCBIN "baserom.gba", 0x1AF028, 1
-
-	.INCBIN "baserom.gba", 0x1AF029, 0x1AF0AC-0x1AF029
-
-	.GLOBAL gCgbFreqTable
-gCgbFreqTable:
-	.INCBIN "baserom.gba", 0x1AF0AC, 1
-
-	.INCBIN "baserom.gba", 0x1AF0AD, 0x1AF0C4-0x1AF0AD
-
-	.GLOBAL gNoiseTable
-gNoiseTable:
-	.INCBIN "baserom.gba", 0x1AF0C4, 1
-
-	.INCBIN "baserom.gba", 0x1AF0C5, 0x1AF100-0x1AF0C5
-
-	.GLOBAL gCgb3Vol
-gCgb3Vol:
-	.INCBIN "baserom.gba", 0x1AF100, 1
-
-	.INCBIN "baserom.gba", 0x1AF101, 0x1AF110-0x1AF101
-
-	.GLOBAL gClockTable
-gClockTable:
-	.INCBIN "baserom.gba", 0x1AF110, 1
-
-	.INCBIN "baserom.gba", 0x1AF111, 0x1AF144-0x1AF111
-
-	.GLOBAL gXcmdTable
-gXcmdTable:
-	.INCBIN "baserom.gba", 0x1AF144, 1
-
-	.INCBIN "baserom.gba", 0x1AF145, 0x1B4288-0x1AF145
-
-	.GLOBAL gMPlayTable
-gMPlayTable:
-	.INCBIN "baserom.gba", 0x1B4288, 1
-
-	.INCBIN "baserom.gba", 0x1B4289, 0x1B42DC-0x1B4289
-
-	.GLOBAL gSongTable
-gSongTable:
-	.INCBIN "baserom.gba", 0x1B42DC, 1
-
-	.INCBIN "baserom.gba", 0x1B42DD, 0x2aba28-0x1B42DD
-
-	.GLOBAL game_text_pack_082aba28 @ houses the compressed game text
-game_text_082aba28:
-	.INCBIN "baserom.gba", 0x2aba28, 0x2d26d0-0x2aba28
-
-	.GLOBAL gaddget_code_082d26d0 @ houses the gaddgets compressed code
-gaddget_code_082d26d0:
-	.INCBIN "baserom.gba", 0x2d26d0, 0x492044-0x2d26d0
-	
-	.GLOBAL gamba_code_08492044 @ houses the gamba games compressed code
-gamba_code_08492044:
-	.INCBIN "baserom.gba", 0x492044, 0x4ed8c0-0x492044
-
-	.GLOBAL koopa_kid_code_084ed8c0 @ houses the koopa kid games compressed code
-koopa_kid_code_084ed8c0:
-	.INCBIN "baserom.gba", 0x4ed8c0, 0x54f2dc-0x4ed8c0
-	
-	.GLOBAL minigame_data_pack_0854f2dc @ houses all the assets for minigames below 
-minigame_data_pack_0854f2dc:
-	.INCBIN "baserom.gba", 0x54f2dc, 0x64d96c-0x54f2dc	
-
-	.GLOBAL minigame_code_0864d96c @ houses the compressed code for minigames that use data from minigame_data_pack_0854f2dc
-minigame_code_0864d96c:
-	.INCBIN "baserom.gba", 0x64d96c, 0x76f2c4-0x64d96c
-	
-	.GLOBAL battle_game_code_0876f2c4 @ houses the compressed code for battle minigames that use data from minigame_data_pack_0854f2dc 
-battle_game_code_0876f2c4:
-	.INCBIN "baserom.gba", 0x76f2c4, 0x7c5434-0x76f2c4
-
-	.GLOBAL game_data_087c5434
-game_data_087c5434:
-	.INCBIN "baserom.gba", 0x7c5434, 0x7c7168-0x7c5434
