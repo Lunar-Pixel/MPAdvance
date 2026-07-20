9b05555d9d2dba52fc6c32e0728d7518dbeb2d93
Lunar-Pixel <83507264+Lunar-Pixel@users.noreply.github.com>
2026-07-19 22:57:54 -0400

    Split minigames and gaddgets

--- data/data_8076AE8.s
+++ data/data_8076AE8.s
@@ -11,131 +11,131 @@ game_data_08076ae8:
 	.GLOBAL gCmprGameGaddgetCode_08077258
 gCmprGameGaddgetCode_08077258:
 	@ Gaddgets
-	.4byte 0x082D26D0	@ snooze ewes gaddget code
+	.4byte gSnoozeEwesCmprCode
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x082D9854	@ portagust gaddget code
-	.4byte 0x082E6AD8	@ magic lamp gaddget code
-	.4byte 0x082F1D44	@ compat i com gaddget code
-	.4byte 0x082FAE9C	@ power star gaddget code
-	.4byte 0x0830246C	@ desktop golf gaddget code
-	.4byte 0x0830B9B8	@ shroom slide gaddget code
-	.4byte 0x083157B4	@ stick to it gaddget code
-	.4byte 0x0832020C	@ bait n wait gaddget code
-	.4byte 0x0832D228	@ digitizer gaddget code
-	.4byte 0x08334BF8	@ lip sync gaddget code
-	.4byte 0x0833FE0C	@ shroom bloom gaddget code
-	.4byte 0x0834B0DC	@ poochy pal gaddget code
-	.4byte 0x08356748	@ bulls eye gaddget code
-	.4byte 0x0835CEDC	@ soil sonar gaddget code
-	.4byte 0x084812F4	@ eye exam gaddget code
-	.4byte 0x083680E8	@ mini slugger gaddget code
-	.4byte 0x0836EAF8	@ tap tap sumo gaddget code
-	.4byte 0x08376FE4	@ egg roll gaddget code
-	.4byte 0x0837F134	@ shroom drop gaddget code
-	.4byte 0x08383ED0	@ morse maker gaddget code
-	.4byte 0x0838E074	@ 4-p pinball gaddget code
-	.4byte 0x08398C70	@ snow globe gaddget code
-	.4byte 0x0839FFC4	@ tile trial gaddget code
-	.4byte 0x083A5ECC	@ map maker gaddget code
-	.4byte 0x083AD030	@ attack frog gaddget code
-	.4byte 0x083B5988	@ mini maze gaddget code
-	.4byte 0x083BAAA4	@ faux flame gaddget code
-	.4byte 0x083C1410	@ stress press gaddget code
-	.4byte 0x083CCA14	@ bomb game gaddget code
-	.4byte 0x08489554	@ card trick gaddget code
-	.4byte 0x083D4C2C	@ hourglass gaddget code
-	.4byte 0x083DBC5C	@ rochambeau gaddget code
-	.4byte 0x083E2958	@ egg panic gaddget code
-	.4byte 0x083ED7E0	@ block punch gaddget code
-	.4byte 0x083F9ADC	@ toad force v gaddget code (cant get to via gaddgets menu)
-	.4byte 0x08400200	@ dart attack gaddget code
-	.4byte 0x0840757C	@ breeze buddy gaddget code
-	.4byte 0x0840D08C	@ chicken race gaddget code
+	.4byte gPortagustCmprCode
+	.4byte gMagicLampCmprCode
+	.4byte gCompatIComCmprCode
+	.4byte gPowerStarCmprCode
+	.4byte gDesktopGolfCmprCode
+	.4byte gShroomSlideCmprCode
+	.4byte gStickToItCmprCode
+	.4byte gBaitNWaitCmprCode
+	.4byte gDigitizerCmprCode
+	.4byte gLipSyncCmprCode
+	.4byte gShroomBloomCmprCode
+	.4byte gPoochyPalCmprCode
+	.4byte gBullseyeCmprCode
+	.4byte gSoilSonarCmprCode
+	.4byte gEyeExamCmprCode
+	.4byte gMiniSluggerCmprCode
+	.4byte gTapTapSumoCmprCode
+	.4byte gEggRollCmprCode
+	.4byte gShroomDropCmprCode
+	.4byte gMorseMakerCmprCode
+	.4byte g4PPinballCmprCode
+	.4byte gSnowGlobeCmprCode
+	.4byte gTileTrialCmprCode
+	.4byte gMapMakerCmprCode
+	.4byte gAttackFrogCmprCode
+	.4byte gMiniMazeCmprCode
+	.4byte gFauxFlameCmprCode
+	.4byte gStressPressCmprCode
+	.4byte gBombGameCmprCode
+	.4byte gCardTrickCmprCode
+	.4byte gHourglassCmprCode
+	.4byte gRochambeauCmprCode
+	.4byte gEggPanicCmprCode
+	.4byte gBlockPunchCmprCode
+	.4byte gToadForceVCmprCode	@ cant get to this via the gaddgets menu, but is accessable (has a unused icon for the gaddgets menu though)
+	.4byte gDartAttackCmprCode
+	.4byte gBreezeBuddyCmprCode
+	.4byte gChickenRaceCmprCode
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x08417E90	@ love me not gaddget code
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x0841F3C8	@ mini bowling gaddget code
-	.4byte 0x08426A44	@ mini b ball gaddget code
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x00000000	@ blank/unused
-	.4byte 0x0842E8E4	@ screen clean gaddget code
-	.4byte 0x084378EC	@ predictotron gaddget code
-	.4byte 0x084401D4	@ cake maker gaddget code
-	.4byte 0x08448E90	@ mini soccer gaddget code
-	.4byte 0x0845214C	@ desert menu gaddget code
-	.4byte 0x0845FDBC	@ bead machine gaddget code
-	.4byte 0x08466410	@ jewelry case gaddget code
-	.4byte 0x0847121C	@ castle night gaddget code
+	.4byte gLoveMeNotCmprCode
 	.4byte 0x00000000	@ blank/unused
 	.4byte 0x00000000	@ blank/unused
 	.4byte 0x00000000	@ blank/unused
 	.4byte 0x00000000	@ blank/unused
 	.4byte 0x00000000	@ blank/unused
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x084788B0	@ hope chest gaddget code
+	.4byte 0x00000000	@ blank/unused
+	.4byte gMiniBowlingCmprCode
+	.4byte gMiniBBallCmprCode
+	.4byte 0x00000000	@ blank/unused
+	.4byte 0x00000000	@ blank/unused
+	.4byte gScreenCleanCmprCode
+	.4byte gPredictotronCmprCode
+	.4byte gCakeMakerCmprCode
+	.4byte gMiniSoccerCmprCode
+	.4byte gDessertMenuCmprCode
+	.4byte gBeadMachineCmprCode
+	.4byte gJewelryCaseCmprCode
+	.4byte gCastleNightCmprCode
+	.4byte 0x00000000	@ blank/unused
+	.4byte 0x00000000	@ blank/unused
+	.4byte 0x00000000	@ blank/unused
+	.4byte 0x00000000	@ blank/unused
+	.4byte 0x00000000	@ blank/unused
+	.4byte 0x00000000	@ blank/unused
+	.4byte gHopeChestCmprCode
 	@ Minigames
-	.4byte 0x0864D96C	@ boo bye game code
-	.4byte 0x08657FC4	@ grabbit game code
-	.4byte 0x08663068	@ chomp walker game code
-	.4byte 0x0866E140	@ cloud climb game code
-	.4byte 0x08677A94	@ barrel peril game code
-	.4byte 0x08680D4C	@ big popper game code
-	.4byte 0x086896DC	@ forest jump game code
-	.4byte 0x08693EA0	@ switch way game code
-	.4byte 0x0869E07C	@ amplifried game code
-	.4byte 0x086A7AE4	@ flingshot game code
-	.4byte 0x086B0F80	@ spooky spike game code
-	.4byte 0x086B9A34	@ bob-ooom game code
-	.4byte 0x086C2AF0	@ reel cheep game code
-	.4byte 0x086CC178	@ shell stack game code
-	.4byte 0x086D4C70	@ bill bounce game code
-	.4byte 0x086DD77C	@ bunny belt game code
-	.4byte 0x086E5C84	@ pest aside game code
-	.4byte 0x086EE3B8	@ melon folly game code
-	.4byte 0x086F7874	@ sort stack game code
-	.4byte 0x08700FB8	@ on the spot game code
+	.4byte gBooByeCmprCode
+	.4byte gGrabbitCmprCode
+	.4byte gChompWalkerCmprCode
+	.4byte gCloudClimbCmprCode
+	.4byte gBarrelPerilCmprCode
+	.4byte gBigPopperCmprCode
+	.4byte gForestJumpCmprCode
+	.4byte gSwitchWayCmprCode
+	.4byte gAmplifriedCmprCode
+	.4byte gFlingshotCmprCode
+	.4byte gSpookySpikeCmprCode
+	.4byte gBobOOOMCmprCode
+	.4byte gReelCheepCmprCode
+	.4byte gShellStackCmprCode
+	.4byte gBillBounceCmprCode
+	.4byte gBunnyBeltCmprCode
+	.4byte gPestAsideCmprCode
+	.4byte gMelonFollyCmprCode
+	.4byte gSortStackCmprCode
+	.4byte gOnTheSpotCmprCode
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x08709AFC	@ koopa crunch game code
-	.4byte 0x08713D6C	@ floor it game code
-	.4byte 0x0871D6B8	@ dreadmill game code
-	.4byte 0x08727194	@ stompbot xl game code
-	.4byte 0x08730F24	@ go go pogo game code
-	.4byte 0x0873B66C	@ sled slide game code
-	.4byte 0x08746410	@ flippin out game code
-	.4byte 0x08750604	@ see monkey minigame code
-	.4byte 0x0875A8E4	@ outta my way game code
-	.4byte 0x08765C3C	@ broom zoom game code
+	.4byte gKoopaKrunchCmprCode
+	.4byte gFloorItCmprCode
+	.4byte gDreadmillCmprCode
+	.4byte gStompbotXLCmprCode
+	.4byte gGoGoPogoCmprCode
+	.4byte gSledSlideCmprCode
+	.4byte gFlippinOutCmprCode
+	.4byte gSeeMonkeyCmprCode
+	.4byte gOuttaMyWayCmprCode
+	.4byte gBroomZoomCmprCode
 	.4byte 0x00000000	@ blank/unused
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x0876F2C4	@ tank down game code
-	.4byte 0x0877C19C	@ hammergeddon game code
-	.4byte 0x08787530	@ stair scare game code
-	.4byte 0x08792294	@ chicken game code
-	.4byte 0x0879B980	@ chain saw game code
-	.4byte 0x087A52A8	@ volleybomb game code
-	.4byte 0x087AFFF8	@ koopa kurl game code
-	.4byte 0x08500A24	@ mush rush game code
-	.4byte 0x084ED8C0	@ crushed ice game code
-	.4byte 0x08512C4C	@ peek-n-seek game code
-	.4byte 0x0852326C	@ splatterball game code
-	.4byte 0x0852FC00	@ trap floor game code
-	.4byte 0x0853F7BC	@ koopa kappa game code
+	.4byte gTankDownCmprCode
+	.4byte gHammergeddonCmprCode
+	.4byte gStairScareCmprCode
+	.4byte gChickenCmprCode
+	.4byte gChainSawCmprCode
+	.4byte gVolleybombCmprCode
+	.4byte gKoopaCurlCmprCode
+	.4byte gMushRushCmprCode
+	.4byte gCrushedIceCmprCode
+	.4byte gPeekNSeekCmprCode
+	.4byte gSplatterballCmprCode
+	.4byte gTrapFloorCmprCode
+	.4byte gKoopaKappaCmprCode
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x08492044	@ scratch em game code
-	.4byte 0x084A1154	@ match em game code
+	.4byte gScratchEmCmprCode
+	.4byte gMatchEmCmprCode
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x084B1768	@ watch em game code
-	.4byte 0x084C1FF4	@ drop em game code
-	.4byte 0x084CF478	@ stop em game code
-	.4byte 0x084DF354	@ pair em game code
+	.4byte gWatchEmCmprCode
+	.4byte gDropEmCmprCode
+	.4byte gStopEmCmprCode
+	.4byte gPairEmCmprCode
 	.4byte 0x00000000	@ blank/unused
-	.4byte 0x087BB1CC	@ slammer game code
+	.4byte gSlammerCmprCode	@ slammer game code
 
 	.GLOBAL game_data_08077448
 game_data_08077448:
@@ -143,7 +143,10 @@ game_data_08077448:
 	
 	.GLOBAL gMenuScrollSpeed_0807F4C0
 gMenuScrollSpeed_0807F4C0:
-	.INCBIN "baserom.gba", 0x7f4c0, 0x7f4c4-0x7f4c0	@ 15 frames, 10 frames, 5 frames , 5 frames
+	.byte 15
+	.byte 10
+	.byte 5
+	.byte 5
 
 	.GLOBAL gGameStateInitFuncs_0807F4C4
 gGameStateInitFuncs_0807F4C4:
--- data/data_82ABA28.s
+++ data/data_82ABA28.s
@@ -9,29 +9,410 @@
 game_text_082aba28:
 	.INCBIN "baserom.gba", 0x2aba28, 0x2d26d0-0x2aba28
 
-	.GLOBAL gaddget_code_082d26d0 @ houses the gaddgets compressed code
-gaddget_code_082d26d0:
-	.INCBIN "baserom.gba", 0x2d26d0, 0x492044-0x2d26d0
-	
-	.GLOBAL gamba_code_08492044 @ houses the gamba games compressed code
-gamba_code_08492044:
-	.INCBIN "baserom.gba", 0x492044, 0x4ed8c0-0x492044
 
-	.GLOBAL koopa_kid_code_084ed8c0 @ houses the koopa kid games compressed code
-koopa_kid_code_084ed8c0:
-	.INCBIN "baserom.gba", 0x4ed8c0, 0x54f2dc-0x4ed8c0
+@ gaddgets
+	.GLOBAL gSnoozeEwesCmprCode
+gSnoozeEwesCmprCode:
+	.INCBIN "subgame/gaddget/snooze_ewes.lz"
+
+	.GLOBAL gPortagustCmprCode
+gPortagustCmprCode:
+	.INCBIN "subgame/gaddget/portagust.lz"
+
+	.GLOBAL gMagicLampCmprCode
+gMagicLampCmprCode:
+	.INCBIN "subgame/gaddget/magic_lamp.lz"
+
+	.GLOBAL gCompatIComCmprCode
+gCompatIComCmprCode:
+	.INCBIN "subgame/gaddget/compat_i_com.lz"
+
+	.GLOBAL gPowerStarCmprCode
+gPowerStarCmprCode:
+	.INCBIN "subgame/gaddget/power_star.lz"
+
+	.GLOBAL gDesktopGolfCmprCode
+gDesktopGolfCmprCode:
+	.INCBIN "subgame/gaddget/desktop_golf.lz"
+
+	.GLOBAL gShroomSlideCmprCode
+gShroomSlideCmprCode:
+	.INCBIN "subgame/gaddget/shroom_slide.lz"
+
+	.GLOBAL gStickToItCmprCode
+gStickToItCmprCode:
+	.INCBIN "subgame/gaddget/stick_to_it.lz"
+
+	.GLOBAL gBaitNWaitCmprCode
+gBaitNWaitCmprCode:
+	.INCBIN "subgame/gaddget/bait_n_wait.lz"
+
+	.GLOBAL gDigitizerCmprCode
+gDigitizerCmprCode:
+	.INCBIN "subgame/gaddget/digitizer.lz"
+
+	.GLOBAL gLipSyncCmprCode
+gLipSyncCmprCode:
+	.INCBIN "subgame/gaddget/lip_sync.lz"
+
+	.GLOBAL gShroomBloomCmprCode
+gShroomBloomCmprCode:
+	.INCBIN "subgame/gaddget/shroom_bloom.lz"
+
+	.GLOBAL gPoochyPalCmprCode
+gPoochyPalCmprCode:
+	.INCBIN "subgame/gaddget/poochy_pal.lz"
+
+	.GLOBAL gBullseyeCmprCode
+gBullseyeCmprCode:
+	.INCBIN "subgame/gaddget/bulls_eye.lz"
+
+	.GLOBAL gSoilSonarCmprCode
+gSoilSonarCmprCode:
+	.INCBIN "subgame/gaddget/soil_sonar.lz"
+
+	.GLOBAL gMiniSluggerCmprCode
+gMiniSluggerCmprCode:
+	.INCBIN "subgame/gaddget/mini_slugger.lz"
+
+	.GLOBAL gTapTapSumoCmprCode
+gTapTapSumoCmprCode:
+	.INCBIN "subgame/gaddget/tap_tap_sumo.lz"
+
+	.GLOBAL gEggRollCmprCode
+gEggRollCmprCode:
+	.INCBIN "subgame/gaddget/egg_roll.lz"
+
+	.GLOBAL gShroomDropCmprCode
+gShroomDropCmprCode:
+	.INCBIN "subgame/gaddget/shroom_drop.lz"
+
+	.GLOBAL gMorseMakerCmprCode
+gMorseMakerCmprCode:
+	.INCBIN "subgame/gaddget/morse_maker.lz"
+
+	.GLOBAL g4PPinballCmprCode
+g4PPinballCmprCode:
+	.INCBIN "subgame/gaddget/4p_pinball.lz"
+
+	.GLOBAL gSnowGlobeCmprCode
+gSnowGlobeCmprCode:
+	.INCBIN "subgame/gaddget/snow_globe.lz"
+
+	.GLOBAL gTileTrialCmprCode
+gTileTrialCmprCode:
+	.INCBIN "subgame/gaddget/tile_trial.lz"
+
+	.GLOBAL gMapMakerCmprCode
+gMapMakerCmprCode:
+	.INCBIN "subgame/gaddget/map_maker.lz"
+
+	.GLOBAL gAttackFrogCmprCode
+gAttackFrogCmprCode:
+	.INCBIN "subgame/gaddget/attack_frog.lz"
+
+	.GLOBAL gMiniMazeCmprCode
+gMiniMazeCmprCode:
+	.INCBIN "subgame/gaddget/mini_maze.lz"
+
+	.GLOBAL gFauxFlameCmprCode
+gFauxFlameCmprCode:
+	.INCBIN "subgame/gaddget/faux_flame.lz"
+
+	.GLOBAL gStressPressCmprCode
+gStressPressCmprCode:
+	.INCBIN "subgame/gaddget/stress_press.lz"
+
+	.GLOBAL gBombGameCmprCode
+gBombGameCmprCode:
+	.INCBIN "subgame/gaddget/bomb_game.lz"
+
+	.GLOBAL gHourglassCmprCode
+gHourglassCmprCode:
+	.INCBIN "subgame/gaddget/hourglass.lz"
+
+	.GLOBAL gRochambeauCmprCode
+gRochambeauCmprCode:
+	.INCBIN "subgame/gaddget/rochambeau.lz"
+
+	.GLOBAL gEggPanicCmprCode
+gEggPanicCmprCode:
+	.INCBIN "subgame/gaddget/egg_panic.lz"
+
+	.GLOBAL gBlockPunchCmprCode
+gBlockPunchCmprCode:
+	.INCBIN "subgame/gaddget/block_punch.lz"
+
+	.GLOBAL gToadForceVCmprCode
+gToadForceVCmprCode:
+	.INCBIN "subgame/gaddget/toad_force_v.lz"
+
+	.GLOBAL gDartAttackCmprCode
+gDartAttackCmprCode:
+	.INCBIN "subgame/gaddget/dart_attack.lz"
+
+	.GLOBAL gBreezeBuddyCmprCode
+gBreezeBuddyCmprCode:
+	.INCBIN "subgame/gaddget/breeze_buddy.lz"
+
+	.GLOBAL gChickenRaceCmprCode
+gChickenRaceCmprCode:
+	.INCBIN "subgame/gaddget/chicken_race.lz"
+
+	.GLOBAL gLoveMeNotCmprCode
+gLoveMeNotCmprCode:
+	.INCBIN "subgame/gaddget/love_me_not.lz"
+
+	.GLOBAL gMiniBowlingCmprCode
+gMiniBowlingCmprCode:
+	.INCBIN "subgame/gaddget/mini_bowling.lz"
+
+	.GLOBAL gMiniBBallCmprCode
+gMiniBBallCmprCode:
+	.INCBIN "subgame/gaddget/mini_b_ball.lz"
+
+	.GLOBAL gScreenCleanCmprCode
+gScreenCleanCmprCode:
+	.INCBIN "subgame/gaddget/screen_clean.lz"
+
+	.GLOBAL gPredictotronCmprCode
+gPredictotronCmprCode:
+	.INCBIN "subgame/gaddget/predictotron.lz"
+
+	.GLOBAL gCakeMakerCmprCode
+gCakeMakerCmprCode:
+	.INCBIN "subgame/gaddget/cake_maker.lz"
+
+	.GLOBAL gMiniSoccerCmprCode
+gMiniSoccerCmprCode:
+	.INCBIN "subgame/gaddget/mini_soccer.lz"
+
+	.GLOBAL gDessertMenuCmprCode
+gDessertMenuCmprCode:
+	.INCBIN "subgame/gaddget/dessert_menu.lz"
+
+	.GLOBAL gBeadMachineCmprCode
+gBeadMachineCmprCode:
+	.INCBIN "subgame/gaddget/bead_machine.lz"
+
+	.GLOBAL gJewelryCaseCmprCode
+gJewelryCaseCmprCode:
+	.INCBIN "subgame/gaddget/jewelry_case.lz"
+
+	.GLOBAL gCastleNightCmprCode
+gCastleNightCmprCode:
+	.INCBIN "subgame/gaddget/castle_night.lz"
+
+	.GLOBAL gHopeChestCmprCode
+gHopeChestCmprCode:
+	.INCBIN "subgame/gaddget/hope_chest.lz"
+
+	.GLOBAL gEyeExamCmprCode
+gEyeExamCmprCode:
+	.INCBIN "subgame/gaddget/eye_exam.lz"
+
+	.GLOBAL gCardTrickCmprCode
+gCardTrickCmprCode:
+	.INCBIN "subgame/gaddget/card_trick.lz"
+
+
+@ coin games
+	.GLOBAL gScratchEmCmprCode
+gScratchEmCmprCode:
+	.INCBIN "subgame/coin_game/scratch_em.lz"
+
+	.GLOBAL gMatchEmCmprCode
+gMatchEmCmprCode:
+	.INCBIN "subgame/coin_game/match_em.lz"
+
+	.GLOBAL gWatchEmCmprCode
+gWatchEmCmprCode:
+	.INCBIN "subgame/coin_game/watch_em.lz"
+
+	.GLOBAL gDropEmCmprCode
+gDropEmCmprCode:
+	.INCBIN "subgame/coin_game/drop_em.lz"
+
+	.GLOBAL gStopEmCmprCode
+gStopEmCmprCode:
+	.INCBIN "subgame/coin_game/stop_em.lz"
+
+	.GLOBAL gPairEmCmprCode
+gPairEmCmprCode:
+	.INCBIN "subgame/coin_game/pair_em.lz"
+
+
+@ koopa kid games
+	.GLOBAL gCrushedIceCmprCode
+gCrushedIceCmprCode:
+	.INCBIN "subgame/koopa_kid/crushed_ice.lz"
+
+	.GLOBAL gMushRushCmprCode
+gMushRushCmprCode:
+	.INCBIN "subgame/koopa_kid/mush_rush.lz"
+
+	.GLOBAL gPeekNSeekCmprCode
+gPeekNSeekCmprCode:
+	.INCBIN "subgame/koopa_kid/peek_n_seek.lz"
+
+	.GLOBAL gSplatterballCmprCode
+gSplatterballCmprCode:
+	.INCBIN "subgame/koopa_kid/splatterball.lz"
+
+	.GLOBAL gTrapFloorCmprCode
+gTrapFloorCmprCode:
+	.INCBIN "subgame/koopa_kid/trap_floor.lz"
+
+	.GLOBAL gKoopaKappaCmprCode
+gKoopaKappaCmprCode:
+	.INCBIN "subgame/koopa_kid/koopa_kappa.lz"
 	
 	.GLOBAL minigame_data_pack_0854f2dc @ houses all the assets for minigames below 
 minigame_data_pack_0854f2dc:
 	.INCBIN "baserom.gba", 0x54f2dc, 0x64d96c-0x54f2dc	
 
-	.GLOBAL minigame_code_0864d96c @ houses the compressed code for minigames that use data from minigame_data_pack_0854f2dc
-minigame_code_0864d96c:
-	.INCBIN "baserom.gba", 0x64d96c, 0x76f2c4-0x64d96c
-	
-	.GLOBAL battle_game_code_0876f2c4 @ houses the compressed code for battle minigames that use data from minigame_data_pack_0854f2dc 
-battle_game_code_0876f2c4:
-	.INCBIN "baserom.gba", 0x76f2c4, 0x7c5434-0x76f2c4
+
+@ minigames that use data from minigame_data_pack_0854f2dc
+	.GLOBAL gBooByeCmprCode
+gBooByeCmprCode:
+	.INCBIN "subgame/minigame/boo_bye.lz"
+
+	.GLOBAL gGrabbitCmprCode
+gGrabbitCmprCode:
+	.INCBIN "subgame/minigame/grabbit.lz"
+
+	.GLOBAL gChompWalkerCmprCode
+gChompWalkerCmprCode:
+	.INCBIN "subgame/minigame/chomp_walker.lz"
+
+	.GLOBAL gCloudClimbCmprCode
+gCloudClimbCmprCode:
+	.INCBIN "subgame/minigame/cloud_climb.lz"
+
+	.GLOBAL gBarrelPerilCmprCode
+gBarrelPerilCmprCode:
+	.INCBIN "subgame/minigame/barrel_peril.lz"
+
+	.GLOBAL gBigPopperCmprCode
+gBigPopperCmprCode:
+	.INCBIN "subgame/minigame/big_popper.lz"
+
+	.GLOBAL gForestJumpCmprCode
+gForestJumpCmprCode:
+	.INCBIN "subgame/minigame/forest_jump.lz"
+
+	.GLOBAL gSwitchWayCmprCode
+gSwitchWayCmprCode:
+	.INCBIN "subgame/minigame/switch_way.lz"
+
+	.GLOBAL gAmplifriedCmprCode
+gAmplifriedCmprCode:
+	.INCBIN "subgame/minigame/amplifried.lz"
+
+	.GLOBAL gFlingshotCmprCode
+gFlingshotCmprCode:
+	.INCBIN "subgame/minigame/flingshot.lz"
+
+	.GLOBAL gSpookySpikeCmprCode
+gSpookySpikeCmprCode:
+	.INCBIN "subgame/minigame/spooky_spike.lz"
+
+	.GLOBAL gBobOOOMCmprCode
+gBobOOOMCmprCode:
+	.INCBIN "subgame/minigame/bob_ooom.lz"
+
+	.GLOBAL gReelCheepCmprCode
+gReelCheepCmprCode:
+	.INCBIN "subgame/minigame/reel_cheep.lz"
+
+	.GLOBAL gShellStackCmprCode
+gShellStackCmprCode:
+	.INCBIN "subgame/minigame/shell_stack.lz"
+
+	.GLOBAL gBillBounceCmprCode
+gBillBounceCmprCode:
+	.INCBIN "subgame/minigame/bill_bounce.lz"
+
+	.INCBIN "subgame/minigame/bunny_belt.lz"
+	.INCBIN "subgame/minigame/pest_aside.lz"
+	.INCBIN "subgame/minigame/melon_folly.lz"
+	.INCBIN "subgame/minigame/sort_stack.lz"
+	.INCBIN "subgame/minigame/on_the_spot.lz"
+
+	.GLOBAL gKoopaKrunchCmprCode
+gKoopaKrunchCmprCode:
+	.INCBIN "subgame/minigame/koopa_krunch.lz"
+
+	.GLOBAL gFloorItCmprCode
+gFloorItCmprCode:
+	.INCBIN "subgame/minigame/floor_it.lz"
+
+	.GLOBAL gDreadmillCmprCode
+gDreadmillCmprCode:
+	.INCBIN "subgame/minigame/dreadmill.lz"
+
+	.GLOBAL gStompbotXLCmprCode
+gStompbotXLCmprCode:
+	.INCBIN "subgame/minigame/stompbot_xl.lz"
+
+	.GLOBAL gGoGoPogoCmprCode
+gGoGoPogoCmprCode:
+	.INCBIN "subgame/minigame/go_go_pogo.lz"
+
+	.GLOBAL gSledSlideCmprCode
+gSledSlideCmprCode:
+	.INCBIN "subgame/minigame/sled_slide.lz"
+
+	.GLOBAL gFlippinOutCmprCode
+gFlippinOutCmprCode:
+	.INCBIN "subgame/minigame/flippin_out.lz"
+
+	.GLOBAL gSeeMonkeyCmprCode
+gSeeMonkeyCmprCode:
+	.INCBIN "subgame/minigame/see_monkey.lz"
+
+	.GLOBAL gOuttaMyWayCmprCode
+gOuttaMyWayCmprCode:
+	.INCBIN "subgame/minigame/outta_my_way.lz"
+
+	.GLOBAL gBroomZoomCmprCode
+gBroomZoomCmprCode:
+	.INCBIN "subgame/minigame/broom_zoom.lz"
+
+
+@ battle games that use data from minigame_data_pack_0854f2dc 
+	.GLOBAL gTankDownCmprCode
+gTankDownCmprCode:
+	.INCBIN "subgame/battle/tank_down.lz"
+
+	.GLOBAL gHammergeddonCmprCode
+gHammergeddonCmprCode:
+	.INCBIN "subgame/battle/hammergeddon.lz"
+
+	.GLOBAL gStairScareCmprCode
+gStairScareCmprCode:
+	.INCBIN "subgame/battle/stair_scare.lz"
+
+	.GLOBAL gChickenCmprCode
+gChickenCmprCode:
+	.INCBIN "subgame/battle/chicken.lz"
+
+	.GLOBAL gChainSawCmprCode
+gChainSawCmprCode:
+	.INCBIN "subgame/battle/chain_saw.lz"
+
+	.GLOBAL gVolleybombCmprCode
+gVolleybombCmprCode:
+	.INCBIN "subgame/battle/volleybomb.lz"
+
+	.GLOBAL gKoopaCurlCmprCode
+gKoopaCurlCmprCode:
+	.INCBIN "subgame/battle/koopa_curl.lz"
+
+	.GLOBAL gSlammerCmprCode
+gSlammerCmprCode:
+	.INCBIN "subgame/battle/slammer.lz"
+
 
 	.GLOBAL game_data_087c5434
 game_data_087c5434:
